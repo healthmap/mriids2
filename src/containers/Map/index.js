b392e166fbcfeea3aa0dd9f120a8b1a0db2f1719
fernandodonaire <fernandodonaire@childrens.harvard.edu>
2020-10-30 13:33:44 -0400

    Added onViewportChange() method to Map component

--- src/containers/Map/index.js
+++ src/containers/Map/index.js
@@ -1,31 +1,44 @@
-import React, {Component} from 'react';
-import ReactMapGL from 'react-map-gl';
+import React, { Component } from "react";
+import ReactMapGL from "react-map-gl";
 
 class Index extends Component {
-    constructor(props) {
-        super(props);
-        this.state =  {
-            viewport: {
-                width: 500,
-                height: 300,
-                latitude: 8.555216,
-                longitude: -11.322184,
-                zoom: 5,
-                minZoom: 2,
-                pitch: 0,
-                bearing: 0
-            }
-        };
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      viewport: {
+        width: "100%",
+        height: "100%",
+        latitude: 8.555216,
+        longitude: -11.322184,
+        zoom: 5,
+        minZoom: 2,
+        pitch: 0,
+        bearing: 0,
+      },
+      settings: {
+        dragPan: true,
+        dragRotate: true,
+        scrollZoom: true,
+        touchZoom: true,
+        touchRotate: true,
+        keyboard: true,
+        doubleClickZoom: true,
+      },
+    };
+  }
 
-    render() {
-        return (
-            <ReactMapGL
-                {...this.state.viewport}
-                onViewportChange={(viewport) => this.setState({viewport})}
-            />
-        );
-    }
+  onViewportChange = (viewport) => {
+    this.setState({ viewport });
+  };
+
+  render() {
+    return (
+      <ReactMapGL
+        {...this.state.viewport}
+        onViewportChange={(viewport) => this.onViewportChange(viewport)}
+      />
+    );
+  }
 }
 
-export default Index
+export default Index;
