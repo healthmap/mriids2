7ad7fe2bed6d911cac9afd0c8cf5fd6258f06aaf
fernandodonaire <fernandodonaire@childrens.harvard.edu>
2020-10-30 13:47:35 -0400

    Added styled-components from original mriids repo

---
+++ src/components/styled-components/Block.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components'
+
+export const BlockPadded = styled.div`
+  padding: 1.3rem;
+  &.reported-cases-wrapper {
+    border-bottom: 1px solid #ccc;
+  }
+`
+
+export const BlockDropshadow = styled.div`
+  display: inline-block;
+  padding: 1rem;
+  box-shadow: 1px 1px 6px 0px rgba(0,0,0,0.10);
+  border-radius: .5rem;
+  background-color: #fff;
+`
