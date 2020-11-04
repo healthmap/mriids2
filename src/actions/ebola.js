7f46d1846c45f96a54af33a7d5fa094a85f37196
fernandodonaire <fernandodonaire@childrens.harvard.edu>
2020-11-04 13:35:55 -0500

    Using d3 to parse the csv ebola data

--- src/actions/ebola.js
+++ src/actions/ebola.js
@@ -1,15 +1,15 @@
-import axios from "axios";
+import * as d3 from "d3-fetch";
 
 export const fetchEbolaData = () => (dispatch) => {
   dispatch({
     type: "FETCH_EBOLA_DATA_REQUEST",
   });
-  return axios
-    .get("csv/healthmap_projections_2018-08-10.csv")
-    .then((response) => {
+  return d3
+    .csv("csv/healthmap_projections_2018-08-10.csv")
+    .then((data) => {
       dispatch({
         type: "FETCH_EBOLA_DATA_SUCCESS",
-        payload: response,
+        payload: data,
       });
     })
     .catch((error) => {
@@ -24,12 +24,12 @@ export const fetchEbolaDataCombined = () => (dispatch) => {
   dispatch({
     type: "FETCH_EBOLA_DATA_COMBINED_REQUEST",
   });
-  return axios
-    .get("csv/healthmap_projections_2018-08-10.csv")
-    .then((response) => {
+  return d3
+    .csv("csv/healthmap_projections_2018-08-10.csv")
+    .then((data) => {
       dispatch({
         type: "FETCH_EBOLA_DATA_COMBINED_SUCCESS",
-        payload: response,
+        payload: data,
       });
     })
     .catch((error) => {
@@ -44,12 +44,12 @@ export const fetchRiskData = () => (dispatch) => {
   dispatch({
     type: "FETCH_RISK_DATA_REQUEST",
   });
-  return axios
-    .get("csv/weighted_flow.csv")
-    .then((response) => {
+  return d3
+    .csv("csv/weighted_flow.csv")
+    .then((data) => {
       dispatch({
         type: "FETCH_RISK_DATA_SUCCESS",
-        payload: response,
+        payload: data,
       });
     })
     .catch((error) => {
