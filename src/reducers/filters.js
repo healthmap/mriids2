eb03b8565e3abe4d221da675b84b5e2bf8deeb98
fernandodonaire <fernandodonaire@childrens.harvard.edu>
2020-11-05 10:46:27 -0500

    Added filters.js reducer

---
+++ src/reducers/filters.js
@@ -0,0 +1,16 @@
+const initialState = {
+  country: "All",
+  outbreak: "Ebola",
+  view: "snapshot",
+  projection: false,
+  dateRange: { from: new Date(2014, 9, 1), to: new Date(2016, 1, 20) },
+};
+
+const Filters = function (state = initialState, action) {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+export default Filters;
