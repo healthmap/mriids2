bee8b07b6273d0056d2db8f2c63b066708cf9fec
rachelchorney <rachelmbertolani@gmail.com>
2020-11-06 10:33:51 -0500

    fixed formatting

--- src/App.js
+++ src/App.js
@@ -1,16 +1,12 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-} from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { ThemeProvider } from "styled-components";
 import { theme } from "./assets/theme";
 import {
-  fetchEbolaData,
-  fetchEbolaDataCombined,
-  fetchRiskData
+    fetchEbolaData,
+    fetchEbolaDataCombined,
+    fetchRiskData,
 } from "./actions/ebola";
 import Map from "./containers/Map";
 import Header from "./components/Header";
@@ -18,44 +14,41 @@ import Sidebar from "./components/Sidebar";
 import { StyledAppContainer } from "./styles";
 
 class App extends Component {
-  componentDidMount() {
-    this.props.fetchEbolaData();
-    this.props.fetchEbolaDataCombined();
-    this.props.fetchRiskData();
-  }
+    componentDidMount() {
+        this.props.fetchEbolaData();
+        this.props.fetchEbolaDataCombined();
+        this.props.fetchRiskData();
+    }
 
-  render() {
-    return (
-      <ThemeProvider theme={theme}>
-        <Router>
-          <StyledAppContainer>
-            <Header />
-            <Switch>
-              <Route exact path="/">
-                <Sidebar />
-                <Map />
-              </Route>
-              <Route exact path="/about">
-                Test
-              </Route>
-              <Route exact path="/team">
-                Test team
-              </Route>
-            </Switch>
-          </StyledAppContainer>
-        </Router>
-      </ThemeProvider>
-    );
-  }
+    render() {
+        return (
+            <ThemeProvider theme={theme}>
+                <Router>
+                    <StyledAppContainer>
+                        <Header />
+                        <Switch>
+                            <Route exact path="/">
+                                <Sidebar />
+                                <Map />
+                            </Route>
+                            <Route exact path="/about">
+                                Test
+                            </Route>
+                            <Route exact path="/team">
+                                Test team
+                            </Route>
+                        </Switch>
+                    </StyledAppContainer>
+                </Router>
+            </ThemeProvider>
+        );
+    }
 }
 
-const mapDispatchToProps = dispatch => ({
-  fetchEbolaData: () => dispatch(fetchEbolaData()),
-  fetchEbolaDataCombined: () => dispatch(fetchEbolaDataCombined()),
-  fetchRiskData: () => dispatch(fetchRiskData())
+const mapDispatchToProps = (dispatch) => ({
+    fetchEbolaData: () => dispatch(fetchEbolaData()),
+    fetchEbolaDataCombined: () => dispatch(fetchEbolaDataCombined()),
+    fetchRiskData: () => dispatch(fetchRiskData()),
 });
 
-export default connect(
-  null,
-  mapDispatchToProps
-)(App);
+export default connect(null, mapDispatchToProps)(App);
