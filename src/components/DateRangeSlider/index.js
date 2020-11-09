f4508ec69a91535470c6785e1d10a29979da751e
fernandodonaire <fernandodonaire@childrens.harvard.edu>
2020-11-09 11:12:48 -0500

    Added DateRangeSliderContainer component

--- src/components/DateRangeSlider/index.js
+++ src/components/DateRangeSlider/index.js
@@ -1,11 +1,16 @@
 import React, { useState } from "react";
 import { connect } from "react-redux";
 import { Slider } from "@material-ui/core";
+import { DateRangeSliderContainer } from "../styled-components/DateRangeSliderContainer";
 
 const DateRangeSlider = (props) => {
   const [numberOfWeeks, setNumberOfWeeks] = useState([20, 68]);
 
-  return <Slider value={numberOfWeeks} />;
+  return (
+    <DateRangeSliderContainer>
+      <Slider value={numberOfWeeks} />
+    </DateRangeSliderContainer>
+  );
 };
 
 const mapStateToProps = (state) => ({
