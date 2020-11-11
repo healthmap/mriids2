e7c5f2fdf051eeb9e02dd0c8c409de6246c35627
fernandodonaire <fernandodonaire@childrens.harvard.edu>
2020-11-11 14:15:56 -0500

    Created changeViewFilter action

--- src/components/ViewToggle/index.js
+++ src/components/ViewToggle/index.js
@@ -1,13 +1,27 @@
 import React from "react";
 import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
+import { changeViewFilter } from "../../actions/filters";
 import ToggleButton from "../ToggleButton";
 import { MapToggleWrapper } from "../styled-components/MapToggleWrapper";
 
 const ViewToggle = (props) => {
+  const currentView = props.view;
+  const toggleFunction = props.changeViewFilter;
   return (
     <MapToggleWrapper>
-      <ToggleButton value="snapshot" label="Snapshot" status={props.view} />
-      <ToggleButton value="risk" label="Risk" status={props.view} />
+      <ToggleButton
+        value="snapshot"
+        label="Snapshot"
+        status={currentView}
+        onClickFunction={toggleFunction}
+      />
+      <ToggleButton
+        value="risk"
+        label="Risk"
+        status={currentView}
+        onClickFunction={toggleFunction}
+      />
     </MapToggleWrapper>
   );
 };
@@ -16,4 +30,12 @@ const mapStateToProps = (state) => ({
   view: state.filters.view,
 });
 
-export default connect(mapStateToProps)(ViewToggle);
+const mapDispatchToProps = (dispatch) =>
+  bindActionCreators(
+    {
+      changeViewFilter,
+    },
+    dispatch
+  );
+
+export default connect(mapStateToProps, mapDispatchToProps)(ViewToggle);
