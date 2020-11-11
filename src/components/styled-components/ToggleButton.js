db7b047b5ac999cd754cf503acaf9b83ba4efdf3
fernandodonaire <fernandodonaire@childrens.harvard.edu>
2020-11-11 15:04:55 -0500

    Updated styling for ToggleButtonStyled component

--- src/components/styled-components/ToggleButton.js
+++ src/components/styled-components/ToggleButton.js
@@ -7,6 +7,8 @@ export const ToggleButtonStyled = styled.button`
   text-transform: uppercase;
   border: 1px solid #000;
   border-top: 0;
+  cursor: pointer;
+  background-color: #fff;
   transition: background-color 0.3s ease;
   &:first-child {
     border-bottom-left-radius: 0.3rem;
