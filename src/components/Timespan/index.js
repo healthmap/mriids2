07bf45198caee797944defd4f0983916f0939460
fernandodonaire <fernandodonaire@childrens.harvard.edu>
2020-11-12 08:27:56 -0500

    Added updateDateRange function to Timespan component

--- src/components/Timespan/index.js
+++ src/components/Timespan/index.js
@@ -1,27 +1,42 @@
 import React from "react";
 import { connect } from "react-redux";
+import dayjs from "dayjs";
+
 import { bindActionCreators } from "redux";
 import { changeDateRange } from "../../actions/filters";
 
 import { TimespanButtonsWrapper } from "../styled-components/TimespanButtonsWrapper";
 import { Button, ButtonLink } from "../styled-components/Button";
+import { ebolaInitialDateRange } from "../../constants/DateRanges";
 
 const Timespan = (props) => {
+  const updateDateRange = (number = 0, unitOfTime) => {
+    //  uses the dayjs library to add units of time to the 'from' date in the initial date range.
+    const newToDate = new Date(
+      dayjs(ebolaInitialDateRange.dateRange.from)
+        .add(number, unitOfTime)
+        .format()
+    );
+    props.changeDateRange([ebolaInitialDateRange.dateRange.from, newToDate]);
+  };
+  const resetDateRange = () => {
+    //  Reset the date range filter to it's initial state
+    props.changeDateRange([
+      ebolaInitialDateRange.dateRange.from,
+      ebolaInitialDateRange.dateRange.to,
+    ]);
+  };
   return (
     <TimespanButtonsWrapper>
-      <Button onClick={() => console.log("1 month")}>1 month</Button>
-      <Button onClick={() => console.log("3 month")}>3 months</Button>
-      <Button onClick={() => console.log("6 month")}>6 months</Button>
-      <Button onClick={() => console.log("1 year")}>1 year</Button>
-      <Button onClick={() => console.log("max")}>Max</Button>
-      <ButtonLink onClick={() => console.log("max")}>Reset</ButtonLink>
+      <Button onClick={() => updateDateRange(1, "month")}>1 month</Button>
+      <Button onClick={() => updateDateRange(3, "month")}>3 months</Button>
+      <Button onClick={() => updateDateRange(6, "month")}>6 months</Button>
+      <Button onClick={() => updateDateRange(1, "year")}>1 year</Button>
+      <Button onClick={() => resetDateRange()}>Max</Button>
+      <ButtonLink onClick={() => resetDateRange()}>Reset</ButtonLink>
     </TimespanButtonsWrapper>
   );
 };
-
-const mapStateToProps = (state) => ({
-  dateRange: state.filters.dateRange,
-});
 
 const mapDispatchToProps = (dispatch) =>
   bindActionCreators(
@@ -31,4 +46,4 @@ const mapDispatchToProps = (dispatch) =>
     dispatch
   );
 
-export default connect(mapStateToProps, mapDispatchToProps)(Timespan);
+export default connect(null, mapDispatchToProps)(Timespan);
