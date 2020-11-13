1af57b5143e1b6a24ccbff4d2a02c30010750c7a
fernandodonaire <fernandodonaire@childrens.harvard.edu>
2020-11-13 09:45:50 -0500

    Created EbolaRiskList component

---
+++ src/components/Sidebar/__tests__/EbolaRiskList.test.js
@@ -0,0 +1,7 @@
+import React from "react";
+import { shallow } from "enzyme";
+import EbolaRiskList from "../EbolaRiskList";
+
+test("renders EbolaRiskList component", () => {
+  shallow(<EbolaRiskList />);
+});
