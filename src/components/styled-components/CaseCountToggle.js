9c1637d576d3f75dd5684e7e7c1ed03267550f81
fernandodonaire <fernandodonaire@childrens.harvard.edu>
2020-11-16 10:20:24 -0500

    Created CaseCountToggle styled components

---
+++ src/components/styled-components/CaseCountToggle.js
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+export const CaseCountToggleWrapper = styled.div`
+  position: absolute;
+  bottom: 1rem;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  margin-top: 1em;
+  padding: 1em 0.5em 0 0.5em;
+  border-top: 1px solid #ccc;
+  cursor: pointer;
+  label {
+    margin-left: 0.8em;
+    line-height: 1.1;
+    color: #666;
+    cursor: pointer;
+  }
+`;
