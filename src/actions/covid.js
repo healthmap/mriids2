fa50a1cce3acdb5270ad9cebc2a6acc3b2fb78e2
fernandodonaire <fernandodonaire@childrens.harvard.edu>
2020-12-18 13:36:16 -0500

    Added fetchCovidCaseCounts action

--- src/actions/covid.js
+++ src/actions/covid.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+import * as d3 from "d3-fetch";
 import * as types from "../constants/ActionTypes";
 import { parseCovidData } from "../utils/covidDataHelpers";
 import { allCountries } from "../constants/Countries";
@@ -44,6 +45,24 @@ export const fetchCovidDataCombined = () => (dispatch) => {
     .catch((error) => {
       dispatch({
         type: types.FETCH_COVID_DATA_COMBINED_FAILURE,
+        error,
+      });
+    });
+};
+
+export const fetchCovidCaseCounts = () => (dispatch) => {
+  dispatch({ type: types.FETCH_COVID_CASE_COUNT_DATA_REQUEST });
+  return d3
+    .csv("csv/covidData/cases_all.csv")
+    .then((data) =>
+      dispatch({
+        type: types.FETCH_COVID_CASE_COUNT_DATA_SUCCESS,
+        payload: data,
+      })
+    )
+    .catch((error) => {
+      dispatch({
+        type: types.FETCH_COVID_CASE_COUNT_DATA_FAILURE,
         error,
       });
     });
