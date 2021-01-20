a26666b84e2d723d8554e6e1792283450ba3ae57
fernandodonaire <fernandodonaire@childrens.harvard.edu>
2021-01-20 10:40:59 -0500

    Connected the ChartTypeButtons component to Redux

---
+++ src/components/ChartTypeButtons/__tests__/ChartTypeButtons.test.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { Provider } from "react-redux";
+import configureStore from "redux-mock-store";
+import thunk from "redux-thunk";
+import renderer from "react-test-renderer";
+import "jest-styled-components";
+import ChartTypeButtons from "../index";
+import { reduxInitialState } from "../../../constants/CommonTestData";
+
+const mockStore = configureStore([thunk]);
+
+describe("Tests for the connected ChartTypeButtons", () => {
+  let store;
+  let component;
+
+  beforeEach(() => {
+    store = mockStore(reduxInitialState);
+
+    component = renderer.create(
+      <Provider store={store}>
+        <ChartTypeButtons />
+      </Provider>
+    );
+  });
+  test("should render with given state from Redux store", () => {
+    expect(component.toJSON()).toMatchSnapshot();
+  });
+});
--- src/components/ChartTypeButtons/index.js
+++ src/components/ChartTypeButtons/index.js
@@ -1,14 +1,39 @@
 import React from "react";
+import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
+import { changeChartType } from "../../actions/filters";
 import { ChartTypeButtonsWrapper } from "../styled-components/ChartTypeButtonsWrapper";
 import { Button } from "../styled-components/Button";
 
-const ChartTypeButtons = () => (
+const ChartTypeButtons = ({ chartType, changeChartType }) => (
   <ChartTypeButtonsWrapper>
     <label>Chart Type</label>
-    <Button>Cases</Button>
-    <Button>Deaths</Button>
-    <Button>Cases and Deaths</Button>
+    <Button
+      className={chartType === "cases" && "is-active"}
+      onClick={() => changeChartType("cases")}
+    >
+      Cases
+    </Button>
+    <Button
+      className={chartType === "deaths" && "is-active"}
+      onClick={() => changeChartType("deaths")}
+    >
+      Deaths
+    </Button>
+    <Button
+      className={chartType === "cases and deaths" && "is-active"}
+      onClick={() => changeChartType("cases and deaths")}
+    >
+      Cases and Deaths
+    </Button>
   </ChartTypeButtonsWrapper>
 );
 
-export default ChartTypeButtons;
+const mapStateToProps = (state) => ({
+  chartType: state.filters.chartType,
+});
+
+const mapDispatchToProps = (dispatch) =>
+  bindActionCreators({ changeChartType }, dispatch);
+
+export default connect(mapStateToProps, mapDispatchToProps)(ChartTypeButtons);
