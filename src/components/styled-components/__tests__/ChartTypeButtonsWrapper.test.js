7c97837529ea00c49714470be5dd2da8b2dcdc52
fernandodonaire <fernandodonaire@childrens.harvard.edu>
2021-01-20 08:45:31 -0500

    Created ChartTypeButtonsWrapper.js styled component

---
+++ src/components/styled-components/__tests__/ChartTypeButtonsWrapper.test.js
@@ -0,0 +1,7 @@
+import React from "react";
+import { shallow } from "enzyme";
+import { ChartTypeButtonsWrapper } from "../ChartTypeButtonsWrapper";
+
+test("render ChartTypeButtonsWrapper", () => {
+  shallow(<ChartTypeButtonsWrapper />);
+});
