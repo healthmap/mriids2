d745bf9f3fa982cc3e676e6fb0bcf480c1dbe60e
rachelchorney <rachelmbertolani@gmail.com>
2021-02-04 14:06:08 -0500

    updated sidebar

--- src/components/Sidebar/styles.js
+++ src/components/Sidebar/styles.js
@@ -1,15 +1,13 @@
 import styled from "styled-components";
 
 export const SidebarWrapper = styled.div`
-    position: absolute;
-    top: ${(props) => props.theme.headerHeight};
-    left: 0;
-    bottom: 0;
-    width: ${(props) => props.theme.sidebarWidth};
-    background-color: #efefef; /* $lightest-gray make variable */
-    overflow: auto;
-    z-index: 2; /* $z-index--sidebar make variable */
-    h4 {
-        padding-top: 1em;
-    }
+  position: absolute;
+  top: ${(props) => props.theme.headerHeight};
+  left: 0;
+  bottom: 0;
+  width: calc(${(props) => props.theme.sidebarWidth} - 3.2rem);
+  padding: 2.4rem 1.6rem;
+  background-color: ${(props) => props.theme.lightestGray};
+  overflow: auto;
+  z-index: 2; /* $z-index--sidebar make variable */
 `;
