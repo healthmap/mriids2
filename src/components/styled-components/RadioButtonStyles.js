264109e1d78daa88e85addd2cd1113a095c5d94f
rachelchorney <rachelmbertolani@gmail.com>
2021-02-15 13:56:36 -0500

    updated sidebar style

--- src/components/styled-components/RadioButtonStyles.js
+++ src/components/styled-components/RadioButtonStyles.js
@@ -1,20 +1,53 @@
 import styled from "styled-components";
-import FormLabel from "@material-ui/core/FormLabel";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import RadioGroup from "@material-ui/core/RadioGroup";
+import Radio from "@material-ui/core/Radio";
 
 export const DataRadioButtonsContainer = styled.div`
   display: block;
-  margin-top: 2rem;
 `;
 
-export const StyledFormLabel = styled(FormLabel)`
-  && {
-    margin: 0 1.2rem 0;
-    font-size: 1.2rem;
+export const InputLabel = styled.label`
+  margin: 2.4rem 1.2rem .4rem;
+  font-size: 1.2rem;
+  line-height: 2;
+  letter-spacing: 0.05rem;
+  text-transform: uppercase;
+  display: block;
+  font-weight: 700;
+  color: ${(props) => props.theme.textColor};
+`;
+
+export const StyledRadioGroup = styled(RadioGroup)`
+  margin-left: 1.2rem;
+`;
+
+export const StyledRadio = styled(Radio)`
+  .MuiSvgIcon-root {
+    width: 2rem;
+    height: 2rem;
+    color: ${(props) => props.theme.secondaryColor};
+  }
+  &.MuiIconButton-colorSecondary:hover,
+  &.MuiRadio-colorSecondary.Mui-checked:hover {
+    background-color: ${(props) => props.theme.secondaryColorTint};
+  }
+  &.MuiRadio-colorSecondary.Mui-checked {
+    color: ${(props) => props.theme.secondaryColorTint};
+  }
+  &.MuiRadio-root {
+    color: ${(props) => props.theme.secondaryColorTintDark};
+  }
+.PrivateSwitchBase-root-6 {
+    padding: 6px;
+}
+`;
+
+export const StyledFormControlLabel = styled(FormControlLabel)`
+  .MuiTypography-body1 {
+    font-size: 1.4rem;
     line-height: 2;
-    letter-spacing: 0.05rem;
-    text-transform: uppercase;
-    display: block;
-    font-weight: 700;
-    color: ${(props) => props.theme.textColor};
+    font-weight: 600;
+	  margin-left: .2rem;
   }
 `;
