d4d3de54fd011f25f5cdae11a1f598eb7e01ebd9
fernandodonaire <fernandodonaire@childrens.harvard.edu>
2021-02-17 14:55:29 -0500

    Updated store to handle large popoverAnchorElement state and action

--- src/store.js
+++ src/store.js
@@ -2,6 +2,25 @@ import { createStore, applyMiddleware } from "redux";
 import { composeWithDevTools } from "redux-devtools-extension";
 import thunk from "redux-thunk";
 import rootReducer from "./reducers/rootReducer";
+
+const sanitizeStateFunction = (state) =>
+  state.ui.popoverAnchorElement
+    ? {
+        ...state,
+        ui: { ...state.ui, popoverAnchorElement: "<<ANCHOR_ELEMENT>>" },
+      }
+    : state;
+
+const sanitizeActionFunction = (action) =>
+  action.type === "SET_POPOVER_ANCHOR_ELEMENT" && action.payload
+    ? { ...action, payload: "<<ANCHOR_ELEMENT>>" }
+    : action;
+
+const composeEnhancers = composeWithDevTools({
+  actionSanitizer: sanitizeActionFunction,
+  stateSanitizer: sanitizeStateFunction,
+});
+
 export default function configureStore() {
-  return createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
+  return createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)));
 }
