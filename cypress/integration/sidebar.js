4415ff75bf5fd6d8e188d3f786af2c425570072d
fernandodonaire <fernandodonaire@childrens.harvard.edu>
2021-02-18 12:21:24 -0500

    Added test for checking correct COVID-19 start date

--- cypress/integration/sidebar.js
+++ cypress/integration/sidebar.js
@@ -11,4 +11,18 @@ describe("Tests for the Sidebar component", () => {
       "Oct 1, 2014 - Feb 20, 2016"
     );
   });
+  it("the open date range button should display 'Jan 1, 2020' when you select the COVID-19 outbreak", () => {
+    // 1. Within the Sidebar component, click on the outbreak select component.
+    cy.react("Sidebar").within(() => {
+      cy.react("Select", { props: { name: "outbreak" } })
+        .eq(1)
+        .click();
+    });
+    // 2. Click on the first option from the Outbreaks ul (COVID-19).
+    cy.get(".MuiList-root").within(() => {
+      cy.get("li").eq(1).click();
+    });
+    //  3. Check to see if the correct date is displayed.
+    cy.get('[data-test-id="open-date-range-button"]').contains("Jan 1, 2020");
+  });
 });
