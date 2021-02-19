d23641e656b954eaf731ccd619d88d1a313a3d1e
fernandodonaire <fernandodonaire@childrens.harvard.edu>
2021-02-19 15:28:23 -0500

    Made ProjectionsPopup a functional component

--- src/components/ProjectionsPopup/index.js
+++ src/components/ProjectionsPopup/index.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 import { connect } from "react-redux";
 import { bindActionCreators } from "redux";
 import { closeProjectionsPopup } from "../../actions/ui";
@@ -10,48 +10,46 @@ import {
 import Button from "@material-ui/core/Button";
 import Modal from "@material-ui/core/Modal";
 
-class ProjectionsPopup extends Component {
-  handleConfirmButtonClick = () => {
-    this.props.changeDataType(
-      this.props.outbreakSelected === "COVID 19"
-        ? "projected deaths"
-        : "projected cases"
+const ProjectionsPopup = ({
+  isProjectionsPopupOpen,
+  outbreakSelected,
+  closeProjectionsPopup,
+  changeDataType,
+}) => {
+  const handleConfirmButtonClick = () => {
+    changeDataType(
+      outbreakSelected === "COVID 19" ? "projected deaths" : "projected cases"
     );
-    this.props.closeProjectionsPopup();
+    closeProjectionsPopup();
   };
-  render() {
-    return (
-      <Modal open={this.props.isProjectionsPopupOpen}>
-        <PopupContainer>
-          <h2>Show Additional Data</h2>
-          <p>
-            Some of our data, including risk and projection data, is for
-            research only.
-          </p>
-          <p>
-            Please click confirm to accept our terms and conditions before
-            viewing this data.
-          </p>
-          <PopupButtonsContainer>
-            <Button
-              variant="contained"
-              onClick={() => this.props.closeProjectionsPopup()}
-            >
-              Cancel
-            </Button>
-            <Button
-              variant="contained"
-              color="primary"
-              onClick={() => this.handleConfirmButtonClick()}
-            >
-              Confirm
-            </Button>
-          </PopupButtonsContainer>
-        </PopupContainer>
-      </Modal>
-    );
-  }
-}
+  return (
+    <Modal open={isProjectionsPopupOpen}>
+      <PopupContainer>
+        <h2>Show Additional Data</h2>
+        <p>
+          Some of our data, including risk and projection data, is for research
+          only.
+        </p>
+        <p>
+          Please click confirm to accept our terms and conditions before viewing
+          this data.
+        </p>
+        <PopupButtonsContainer>
+          <Button variant="contained" onClick={() => closeProjectionsPopup()}>
+            Cancel
+          </Button>
+          <Button
+            variant="contained"
+            color="primary"
+            onClick={() => handleConfirmButtonClick()}
+          >
+            Confirm
+          </Button>
+        </PopupButtonsContainer>
+      </PopupContainer>
+    </Modal>
+  );
+};
 
 const mapStateToProps = (state) => ({
   outbreakSelected: state.filters.outbreak,
