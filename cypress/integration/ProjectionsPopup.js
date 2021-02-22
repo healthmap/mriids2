89add9b9db35a767500fce1ce6804c42f46dd478
fernandodonaire <fernandodonaire@childrens.harvard.edu>
2021-02-22 11:46:29 -0500

    Added cypress test for when modal is closed

--- cypress/integration/ProjectionsPopup.js
+++ cypress/integration/ProjectionsPopup.js
@@ -3,15 +3,27 @@ describe("Tests for the projections popup modal", () => {
     cy.visit("localhost:3000");
     cy.waitForReact();
   });
-  it("should not be visible by default", () => {
+  it("modal contents should not be visible by default", () => {
     cy.contains("Show Additional Data").should("not.exist");
   });
+
   it("should display modal contents when clicking on 'Projected Cases'", () => {
-    // 1. Click on Projected Cases radio buttons (second radio button).
+    // 1. Click on Projected Cases radio buttons.
     cy.react("DataRadioButtons").within(() => {
-      cy.get(".MuiFormControlLabel-root").eq(1).click();
+      cy.contains("Projected Cases").click();
     });
     // 2. Modal title should now be visible.
     cy.contains("Show Additional Data").should("exist");
+  });
+
+  it("should close the modal when clicking on 'Cancel' button", () => {
+    // 1. Click on Projected Cases radio button to open the modal.
+    cy.react("DataRadioButtons").within(() => {
+      cy.contains("Projected Cases").click();
+    });
+    // 2. Click on 'Cancel' button.
+    cy.contains("Cancel").click();
+    // 3. Modal title should no longer be visible.
+    cy.contains("Show Additional Data").should("not.exist");
   });
 });
