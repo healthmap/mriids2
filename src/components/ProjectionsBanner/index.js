4efc26ddcbb976f36d65135b588617bea1c8c5ea
fernandodonaire <fernandodonaire@childrens.harvard.edu>
2021-02-25 13:52:49 -0500

    Created ProjectionBanner component

---
+++ src/components/ProjectionsBanner/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { ProjectionsBannerContainer } from "./styles";
+
+const ProjectionBanner = () => (
+  <ProjectionsBannerContainer>
+    You are viewing data that is for research only.
+  </ProjectionsBannerContainer>
+);
+
+export default ProjectionBanner;
---
+++ src/components/ProjectionsBanner/styles.js
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const ProjectionsBannerContainer = styled.div`
+  display: flex;
+  height: 40px;
+  justify-content: center;
+  align-items: center;
+`;
