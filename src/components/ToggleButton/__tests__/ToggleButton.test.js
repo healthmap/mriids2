6d58b0accfce884a7ca911d52c5c6f708763f6a1
fernandodonaire <fernandodonaire@childrens.harvard.edu>
2021-03-11 15:51:36 -0500

    Deleted unused ToggleButton component

--- src/components/ToggleButton/__tests__/ToggleButton.test.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ToggleButton from "../index";
-import { shallow } from "enzyme";
-
-describe("Tests for the ToggleButton component", () => {
-  test("renders ToggleButton component", () => {
-    shallow(<ToggleButton />);
-  });
-});
