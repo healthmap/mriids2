7f001dfa5e30b52c48930466771e7f60c9d0c42a
fernandodonaire <fernandodonaire@childrens.harvard.edu>
2021-03-11 15:13:34 -0500

    Deleted unused ModalTitle styled-component

--- src/components/styled-components/ModalTitle.js
+++
@@ -1,7 +0,0 @@
-import styled from "styled-components";
-
-export const ModalTitle = styled.h4`
-  font-size: 16px;
-  line-height: 1.428;
-  margin: 0;
-`;
--- src/components/styled-components/__tests__/ModalTitle.test.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import { shallow } from "enzyme";
-import { ModalTitle } from "../ModalTitle";
-
-test("render ModalTitle", () => {
-  shallow(<ModalTitle />);
-});
