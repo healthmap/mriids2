1269f0a26bcf73776bd8e2e42083780b5644fe29
fernandodonaire <fernandodonaire@childrens.harvard.edu>
2021-03-11 15:10:04 -0500

    Deleted unused MarkerStyled styled-component

--- src/components/styled-components/MarkerStyled.js
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components";
-import { Marker } from "react-map-gl";
-
-export const MarkerStyled = styled(Marker)`
-  width: 8px;
-  height: 8px;
-  background-color: red;
-  border-radius: 50%;
-`;
